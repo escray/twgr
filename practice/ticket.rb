3dd721587915f6374ac55c0065a2e5c3096ecf1c
escray <zescray@gmail.com>
2020-03-03 09:24:28 +0800

    setter method

--- practice/ticket.rb
+++ practice/ticket.rb
@@ -1,8 +1,7 @@
 class Ticket
-  def initialize(venue, date)
+  def initialize(venue)
     puts "Creating a new ticket!"
     @venue = venue
-    @date = date
   end
 
   def venue
@@ -13,16 +12,35 @@ class Ticket
     @date
   end
 
+  def date=(date)
+    year, month, day = date.split('-')
+    if year.to_i < 100
+      puts "Please submit the date in the format 'yyyy-mm-dd'."
+    end
+    @date = date
+  end
+
   def price
     @price
   end
 
+
   def price=(amount)
-    @price = amount
+    if (amount * 100).to_i == amount * 100
+      @price = amount
+    else
+      puts "The price seems to be malformed"
+    end
+
   end
 
   def discount(percent)
-    @price = @price * (100 - percent) / 100.0
+    if (percent * 100).to_i == percent * 100 && percent <= 100
+      @price = @price * (100 - percent) / 100.0
+    else
+      puts "The discount seems to be malformed"
+    end
+
   end
 
   def event
@@ -30,13 +48,18 @@ class Ticket
   end
 end
 
-th = Ticket.new("Town Hall", "2013-11-12")
+th = Ticket.new("Town Hall")
+th.date = "2013-11-12"
 th.price=(63.00)
-cc = Ticket.new("Convention Center", "2014-12-13")
-cc.price= 64.00
+cc = Ticket.new("Convention Center")
+cc.date = "2014-12-13"
+
 puts "We've created two tickets."
 puts "The first is for a #{th.venue} event on #{th.date}"
 puts "The ticket costs $#{"%.2f" % th.price}"
 th.price=(72.50)
 puts "Whoops -- it just went up. It now costs $#{"%.2f" % th.price}."
+
+cc.price= 64.00
 puts "The second is for an event on #{cc.venue} event on #{cc.date}"
+puts "The ticket for #{cc.venue} has been discounted 15% to #{th.discount(15)}"
---
+++ practice/travelagent.rb
@@ -0,0 +1,8 @@
+class TravelAgentSession
+  def year=(y)
+    @year = y.to_i
+    if @year < 100
+      @year = @year + 2000
+    end
+  end
+end
