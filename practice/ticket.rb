10a74eb1561b6bbbc987bb0ec430e6ef5d7a1862
escray <zescray@gmail.com>
2020-03-03 09:30:10 +0800

    symbols

--- practice/ticket.rb
+++ practice/ticket.rb
@@ -1,15 +1,10 @@
 class Ticket
-  def initialize(venue)
-    puts "Creating a new ticket!"
+  attr_reader :venue, :date, :price
+  attr_writer :price
+
+  def initialize(venue, date)
     @venue = venue
-  end
-
-  def venue
-    @venue
-  end
-
-  def date
-    @date
+    @date = date
   end
 
   def date=(date)
@@ -20,18 +15,12 @@ class Ticket
     @date = date
   end
 
-  def price
-    @price
-  end
-
-
   def price=(amount)
     if (amount * 100).to_i == amount * 100
       @price = amount
     else
       puts "The price seems to be malformed"
     end
-
   end
 
   def discount(percent)
@@ -47,19 +36,3 @@ class Ticket
     "Can't really be specified yet ..."
   end
 end
-
-th = Ticket.new("Town Hall")
-th.date = "2013-11-12"
-th.price=(63.00)
-cc = Ticket.new("Convention Center")
-cc.date = "2014-12-13"
-
-puts "We've created two tickets."
-puts "The first is for a #{th.venue} event on #{th.date}"
-puts "The ticket costs $#{"%.2f" % th.price}"
-th.price=(72.50)
-puts "Whoops -- it just went up. It now costs $#{"%.2f" % th.price}."
-
-cc.price= 64.00
-puts "The second is for an event on #{cc.venue} event on #{cc.date}"
-puts "The ticket for #{cc.venue} has been discounted 15% to #{th.discount(15)}"
