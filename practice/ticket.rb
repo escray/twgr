77d3475185f977f2493488526174c920a356c2db
escray <zescray@gmail.com>
2020-03-03 09:06:22 +0800

    price

--- practice/ticket.rb
+++ practice/ticket.rb
@@ -1,8 +1,9 @@
 class Ticket
-  def initialize(venue, date)
+  def initialize(venue, date, price)
     puts "Creating a new ticket!"
     @venue = venue
     @date = date
+    @price = price
   end
 
   def venue
@@ -13,13 +14,21 @@ class Ticket
     @date
   end
 
+  def price
+    @price
+  end
+
+  def discount(percent)
+    @price = @price * (100 - percent) / 100.0
+  end
+
   def event
     "Can't really be specified yet ..."
   end
 end
 
-th = Ticket.new("Town Hall", "2013-11-12")
-cc = Ticket.new("Convention Center", "2014-12-13")
+th = Ticket.new("Town Hall", "2013-11-12", 63.00)
+cc = Ticket.new("Convention Center", "2014-12-13", 64.00)
 puts "We've created two tickets."
 puts "The first is for a #{th.venue} event on #{th.date}"
 puts "The second is for an event on #{cc.venue} event on #{cc.date}"
