810411a595df591d6c25f0a93e5a9caf8838b9ee
escray <zescray@gmail.com>
2020-03-04 11:06:36 +0800

    friends

--- practice/person.rb
+++ practice/person.rb
@@ -1,4 +1,34 @@
 class Person
+  PEOPLE = []
+  attr_reader :name, :hobbies, :friends
+  def initialize(name)
+    @name = name
+    @hobbies = []
+    @friends = []
+    PEOPLE << self
+  end
+  def has_hobby(hobby)
+    @hobbies << hobby
+  end
+  def has_friend(friend)
+    @friends << friend
+  end
+  def Person.method_missing(m, *args)
+    method = m.to_s
+    if method.start_with?("all_with_")
+      attr = method[9..-1]
+      if Person.public_method_defined?(attr)
+        PEOPLE.find_all do |person|
+          person.send(attr).include?(args[0])
+        end
+      else
+        raise ArgumentError, "Can't find #{attr}"
+      end
+    else
+      super
+    end
+  end
+
   def species
     "Homo sapiens"
   end
@@ -18,9 +48,24 @@ class Rubyist < Person
 
 end
 
-joe = Person.new
-joe.set_name("Joe")
+joe = Person.new("Joe")
 puts joe.get_name
 
-david = Rubyist.new
+david = Rubyist.new("David")
 puts david.species
+
+j = Person.new("John")
+p = Person.new("Paul")
+g = Person.new("George")
+r = Person.new("Ringo")
+j.has_friend(p)
+j.has_friend(g)
+g.has_friend(p)
+r.has_hobby("rings")
+
+Person.all_with_friends(p).each do |person|
+  puts "#{person.name} is friends with #{p.name}"
+end
+Person.all_with_hobbies("rings").each do |person|
+  puts "#{person.name} is into rings"
+end
