e35bc89e091d569e38fc523f09e703afa8d2281d
escray <zescray@gmail.com>
2020-03-08 16:38:34 +0800

    true false

--- practice/inspect.rb
+++ practice/inspect.rb
@@ -1,9 +1,16 @@
 class Person
+  attr_accessor :name, :age, :email
   def initialize(name)
     @name = name
   end
   def inspect
     @name
+  end
+  def to_str
+    name
+  end
+  def to_ary
+    [name, age, email]
   end
 end
 
@@ -18,5 +25,14 @@ names = ["David", "Black"]
 puts combine_names(*names)
 
 print "Enter a number:"
-n = gets.chomp
+n = gets.chomp.to_i
 puts n * 100
+
+david.name = "David"
+david.age = 55
+david.email = "david@wherever"
+array = []
+array.concat(david)
+puts "david is named " + david + "."
+p array
+p david.to_a
